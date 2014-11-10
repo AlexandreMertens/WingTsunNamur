b16c963693b5ee64679e9b5e01f78f0f9bd8d80d
Alexandre Mertens <amertens@cp3lap19.fynu.ucl.ac.be>
2014-11-10 10:44:57 +0100

    new style.css

--- www/index.php
+++ www/index.php
@@ -7,27 +7,43 @@
 
 <body>
 
-  <?php include("includes/contact.php"); ?>
+<div id="wrapper" style="background-color:#aaa;">
+
+  <?php include("includes/ewto.php"); ?>
+<!--  <?php include("includes/contact.php"); ?> -->
   <?php include("includes/header.php"); ?>
   <?php include("includes/nav.php"); ?>
 
+<div id="content">
   <div id="content-main">
 	<p>L'école de Wing Tsun et d'escrima de Namur a été créée en 2001. L'enseignement est dispensé par Sifu Rudi Vaneetvelde qui est aujourd'hui le pratiquant le plus haut gradé en wallonie. </p>
+
+	<h2>Actualités</h2>
+	<div id="event">
+                <iframe style="float: left; margin: 0px 15px 0px 0px;" width=200px src="http://www.youtube.com/embed/lFuQdoXX_gg">
+		</iframe> 
+                <p><i>wing-tsun</i></p>
+                <h3> 27 Décembre: Stage Siu-Nim-Tao </h3>
+                <p> Stage organisé par Sifu Rudi Vaneetvelde. Le stage portera sur les applications en combat des mouvements de la forme, en commençant par la fin. </p>
+        </div>
+
 	<h2>News</h2>
 
         <div id="event">
-		<img style="float: left; margin: 0px 0px 0px 0px;" src="./photo1.jpg" width=30% />
-		<p>wing-tsun et chi-kung</p>
+	  	<img style="float: left; margin: 0px 15px 0px 0px;" src="./figures/Grece.jpg" width=200px /> 
+	 	<p><i>wing-tsun et chi-kung</i></p>
                 <h3> Septembre: Stage en Grèce avec Sifu Michael Schwarz </h3>
                 <p> Stage organisé par Sifu Michael Schwarz (7e grade Maître et entraineur national de la Belgique francophone et du Luxembourg) en Grèce. Sept jours d'entrainement et de vacances au bord de la mer Égée. </p>
         </div>
 	<div id="event">
-		<img style="float: left; margin: 0px 0px 0px 0px;" src="./Grece.jpg" width=30% />
-		 <p>wing-tsun</p>
-		<h3> 18 mai: Stage/Passage de grade avec Sifu Michael Schwarz </h3>
-		<p> Stage avec Sifu Michael Schwarz (7e grade Maître et entraineur national de la Belgique francophone et du Luxembourg) du 18 mai. Six heures d'entrainement sous un soleil de plomb. </p>
+		<img style="float: left; margin: 0px 15px 0px 0px;" src="./figures/photo1.jpg" width= 200px />
+		 <p><i>wing-tsun</i></p>
+		<h3> 18 mai: Séminaire/Passage de grade avec Sifu Michael Schwarz </h3>
+		<p> Séminaire par Sifu Michael Schwarz (7e grade Maître et entraineur national de la Belgique francophone et du Luxembourg) du 18 mai. Six heures d'entrainement sous un soleil de plomb. </p>
 	</div>
   </div>
+  <?php include("includes/agenda.php"); ?>
+</div>
 
   <div id="footer">2014 - Organisation Wallone et Luxembourgeoise de Wing Tsun (OWLWT)</div>
   <div id="bottom"></div>
