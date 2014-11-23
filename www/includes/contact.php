a9970e3f4519aca5ecd68bf661ef7baa6293f228
jrekier <jrekier@gmail.com>
2014-11-23 22:37:50 +0100

    rebuild with css framework
    added pictures, calendar, infos, …

--- www/includes/contact.php
+++
@@ -1,7 +0,0 @@
-<div id="contact">
-      	<b id="contact">
-          contacts:<br>
-          Sifu R. Vaneetvelde:<br>
-	</b>
-        <a id="contact" href="mailto:wingtsun.rv@skynet.be?Subject=demande%20 d'infos">wingtsun.rv@skynet.be</a>
-</div>
--- www/includes/footer.php
+++
@@ -1,1 +0,0 @@
-<div id="footer">2014 - Organisation Wallone et Luxembourgeoise de Wing Tsun (OWLWT)</div>
